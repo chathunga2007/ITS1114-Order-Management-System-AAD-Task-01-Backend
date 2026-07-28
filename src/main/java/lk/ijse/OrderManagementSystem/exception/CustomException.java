109fd740040172c03b98642f32ea74dbd22d3087
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-07-29 01:09:13 +0530

    Exception Handle Part Added!

--- src/main/java/lk/ijse/OrderManagementSystem/exception/CustomException.java
+++ src/main/java/lk/ijse/OrderManagementSystem/exception/CustomException.java
@@ -3,11 +3,13 @@ package lk.ijse.OrderManagementSystem.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
-public class CustomerException extends RuntimeException{
+@NoArgsConstructor
+public class CustomException extends RuntimeException {
     private int code;
     private String message;
 }
